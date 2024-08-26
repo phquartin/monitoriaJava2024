e0b94722e9450e8023c8c854d3bc6c2248a21049
Pedro <phquartinmatos@sempreceub.com>
2024-08-26 20:58:29 -0300

    Lista 02

--- src/App.java
+++ src/App.java
@@ -1,63 +1,119 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("O número do exercício (1:30): ");
-        int x = sc.nextInt();
-        switch (x){
-            case 1:
-                System.out.println(Main.Ex01());
-                break;
-            case 2:
-                System.out.println(Main.Ex02());
-                break;
-            case 3:
-                System.out.println("IMC: " + Main.Ex03());
-                break;
-            case 4:
-                System.out.println("Média Ponderada: " + Main.Ex04());
-                break;
-            case 5:
-                System.out.println("Você possui aproximadamente " + Main.Ex05() + " dias de vida");
-                break;
-            case 6:
-                System.out.println("Seu novo salário: " + Main.Ex06());
-                break;
-            case 7:
-                System.out.println(Main.Ex07());
-                break;
-            case 8:
-                System.out.println(Main.Ex08());
-                break;
-            case 9:
-                System.out.println(Main.Ex09());
-                break;
-            case 10:
-                System.out.println(Main.Ex10());
-                break;
-            case 11:
-                System.out.println(Main.Ex11());
-                break;
-            case 12:
-                System.out.println(Main.Ex12());
-                break;
-            case 13:
-                System.out.println(Main.Ex13());
-                break;
-            case 14:
-                System.out.println(Main.Ex14());
-                break;
-            case 15:
-                System.out.println(Main.Ex15());
-                break;
-            case 16:
-                System.out.println(Main.Ex16());
-                break;
-            case 17:
-                System.out.println(Main.Ex17());
-                break;
-        }
-
+        String loop;
+        boolean boo = true;
+        do {
+            System.out.println("O número do exercício (1:30): ");
+            int x = sc.nextInt();
+            switch (x) {
+                case 1:
+                    System.out.println(Main.Ex01());
+                    break;
+                case 2:
+                    System.out.println(Main.Ex02());
+                    break;
+                case 3:
+                    System.out.println("IMC: " + Main.Ex03());
+                    break;
+                case 4:
+                    System.out.println("Média Ponderada: " + Main.Ex04());
+                    break;
+                case 5:
+                    System.out.println("Você possui aproximadamente " + Main.Ex05() + " dias de vida");
+                    break;
+                case 6:
+                    System.out.println("Seu novo salário: " + Main.Ex06());
+                    break;
+                case 7:
+                    System.out.println(Main.Ex07());
+                    break;
+                case 8:
+                    System.out.println(Main.Ex08());
+                    break;
+                case 9:
+                    System.out.println(Main.Ex09());
+                    break;
+                case 10:
+                    System.out.println(Main.Ex10());
+                    break;
+                case 11:
+                    System.out.println(Main.Ex11());
+                    break;
+                case 12:
+                    System.out.println(Main.Ex12());
+                    break;
+                case 13:
+                    System.out.println(Main.Ex13());
+                    break;
+                case 14:
+                    System.out.println(Main.Ex14());
+                    break;
+                case 15:
+                    System.out.println(Main.Ex15());
+                    break;
+                case 16:
+                    System.out.println(Main.Ex16());
+                    break;
+                case 17:
+                    System.out.println(Main.Ex17());
+                    break;
+                case 18:
+                    System.out.println(Main.Ex18());
+                    break;
+                case 19:
+                    System.out.println(Main.Ex19());
+                    break;
+                case 20:
+                    System.out.println(Main.Ex20());
+                    break;
+                case 21:
+                    System.out.println(Main.Ex21());
+                    break;
+                case 22:
+                    System.out.println(Main.Ex22());
+                    break;
+                case 23:
+                    System.out.println(Main.Ex23());
+                    break;
+                case 24:
+                    System.out.println(Main.Ex24());
+                    break;
+                case 25:
+                    System.out.println(Main.Ex25());
+                    break;
+                case 26:
+                    System.out.println(Main.Ex26());
+                    break;
+                case 27:
+                    System.out.println(Main.Ex27());
+                    break;
+                case 28:
+                    System.out.println(Main.Ex28());
+                    break;
+                case 29:
+                    System.out.println(Main.Ex29());
+                    break;
+                case 30:
+                    System.out.println(Main.Ex30());
+                    break;
+                default:
+                    System.out.println("Digite um exercício válido!");
+            }
+            System.out.println("Deseja fazer mais um exercício?(S/N): ");
+            loop = sc.next();
+            if (loop.equalsIgnoreCase("s")) {
+                System.out.println("Continuando...");
+            }else if (loop.equalsIgnoreCase("n")) {
+                boo = false;
+            }else{
+                System.out.println("Erro na digitação!\n");
+                System.out.println("Deseja fazer mais um exercício?(S/N): ");
+                loop = sc.next();
+            }
+        }while(boo);
+        System.out.println("Fim!");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Thread.sleep;
+
 public class Main {
 
     public static int Ex01() {
@@ -14,12 +16,13 @@ public class Main {
     }
     public static String Ex02() {
         Scanner sc = new Scanner(System.in);
+        String str;
         System.out.println("Digite um número: ");
         int num1 = sc.nextInt();
         int x = num1 * 2;
         int y = num1 * 3;
         int z = num1 * 4;
-        String str = x + " " + y + " " + z;
+        str = x + " " + y + " " + z;
         return str;
     }
     public static float Ex03() {
@@ -176,5 +179,184 @@ public class Main {
         double salario = sc.nextDouble();
         str = (salario >= 1000) ? "Seu novo sálario é " + ((salario * 0.05) + salario) : "Seu novo salário é " + ((salario * 0.1) + salario);
         return str;
+    }
+    public static String Ex18(){
+        Scanner sc = new Scanner(System.in);
+        boolean whie = true;
+        String str;
+        str = "Erro";
+        do{
+            System.out.println("Digite um número entre 1&7: ");
+            int dia = sc.nextInt();
+            switch (dia) {
+                case 1:
+                    whie = false;
+                    str = "Domingo";
+                    break;
+                case 2:
+                    whie = false;
+                    str = "Segunda";
+                    break;
+                case 3:
+                    whie = false;
+                    str = "Terca";
+                    break;
+                case 4:
+                    whie = false;
+                    str = "Quarta";
+                    break;
+                case 5:
+                    whie = false;
+                    str = "Quinta";
+                    break;
+                case 6:
+                    whie = false;
+                    str = "Sexta";
+                    break;
+                case 7:
+                    whie = false;
+                    str = "Sabado";
+                    break;
+                default:
+                    str = "Erro";
+                    System.out.println("Digite um número válido!");
+                    break;
+
+            }
+        }while(whie);
+        return str;
+    }
+    public static String Ex19(){
+        Scanner sc = new Scanner(System.in);
+        String str;
+        float[] num = new float[3];
+        float menorNumero = 0, maiorNumero = 0, medioNumero = 0;
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Digite o numero " + (i + 1) + ": ");
+            num[i] = sc.nextFloat();
+        }for (int i = 0 ; i < 3; i++) {
+            menorNumero = num[0];
+            maiorNumero = num[0];
+            if (num[i] < menorNumero) {
+                menorNumero = num[i];
+            } else if (num[i] > maiorNumero) {
+                maiorNumero = num[i];
+            }
+        }for (int i = 0 ; i < 3; i++)
+            if(maiorNumero != num[i] && menorNumero != num[i])
+                medioNumero = num[i];
+    return "Os números em ordem crescente são: " + menorNumero + ", " + medioNumero +", "+ maiorNumero;
+    }
+    public static String Ex20(){
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        System.out.println("Digite sua idade: ");
+        int idade = sc.nextInt();
+        if (idade >= 18) {
+            str = "Adulto";
+        }else if (idade >= 14) {
+            str = "Juvenil";
+        }else if (idade >= 10) {
+            str = "Infantil";
+        } else{
+            str = "Mirim";
+        }
+        return str;
+    }
+    public static String Ex21() throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("Contagem Regressiva!");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+            sleep(500);
+        }
+        return " ";
+    }
+    public static String Ex22(){
+        for(int i = 2 ; i <= 20 ; i++)
+            if (i % 2 == 0)
+                System.out.println(i);
+        return " ";
+    }
+    public static String Ex23(){
+        for(int i = 1 ; i <= 20 ; i++)
+            if (i % 2 == 1)
+                System.out.println(i);
+        return " ";
+    }
+    public static String Ex24(){
+        for(int i = 0 ; i <= 30 ; i++)
+            if (i % 3 == 0)
+                System.out.println(i);
+        return " ";
+    }
+    public static String Ex25() throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("Contagem Decressiva!");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+            sleep(500);
+        }
+        return " ";
+    }
+    public static String Ex26(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número para descobrir sua tabuada: ");
+        int num = sc.nextInt();
+        for (int i = 0 ; i <= 10 ; i++)
+            System.out.println(num + " x "+ i + " = " + (num * i));
+        return " ";
+    }
+    public static String Ex27(){
+        for(int i = 0 ; i <= 100 ; i++){
+            if (i % 3 == 0 & i % 2 == 0) {
+                System.out.println("FizzBuzz");
+            }else if (i % 3 == 0){
+                System.out.println("Fizz");
+            }else if (i % 2 == 0){
+                System.out.println("Buzz");
+            }else
+                System.out.println(i);
+        }
+        return " ";
+    }
+    public static String Ex28(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número para saber seus divisores: ");
+        int num = sc.nextInt();
+        for(int i = 1 ; i <= num ; i++){
+            if(num % i == 0){
+                System.out.println(i);
+            }
+        }
+    return " ";
+    }
+    public static String Ex29(){
+        Scanner sc = new Scanner(System.in);
+        int num, i = 0, soma = 0;
+        do{
+            System.out.println("(" + i + " Números digitados) | Digite um número e descubra a média deles! (0 para sair): ");
+            num = sc.nextInt();
+            if (num != 0) {
+                i++;
+                soma = soma + num;
+            }
+        }while(num != 0);
+        return "A média é: "+ (soma / i);
+    }
+    public static String Ex30(){
+        Scanner sc = new Scanner(System.in);
+        int num = -1, maiornum = 0;
+        for (int i = 0; num != 0; i++) {
+            System.out.println("Digite um número (0 para sair): ");
+            num = sc.nextInt();
+            if (i == 0){
+                maiornum = num;
+            }else if (num > maiornum)
+                maiornum = num;
+        }
+        return "O maior número é: " + maiornum;
     }
 }
